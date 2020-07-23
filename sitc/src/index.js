fcea475b442fec995445c87c734fd1f36251a6ff
Chris Forrest <redrider_cr@yahoo.com>
2020-07-22 22:00:15 -0400

    made the store in index.js and initial blank reducer

--- sitc/src/index.js
+++ sitc/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+/*******************Chris's imports for redux  ***************************************/
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer } from './reducers';
+import logger from 'redux-logger';
+
+/***************** Chris create store  ************************************************/
+
+const store = createStore( reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
+  <Provider store={store} >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ sitc/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    isFetching: false,
+    admin:'',
+    volunteer:'',
+    student:'',
+    error:''
+}
+
+export const reducer = (state = initialState, action ) => {
+    switch(action.type){
+        default:
+            return state
+    }
+};
