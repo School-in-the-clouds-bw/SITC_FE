8236d8d0e0c0dd0ce2125a32b9a3945cb91ff8da
Asha Gross <hello@ashagross.com>
2020-07-22 21:42:16 -0400

    Login Page shell complete

---
+++ sitc/src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Login = () => {
+    return (
+        <div>
+            <form>
+                <label>
+                    Username:
+                    <input />
+                </label>
+                <label>
+                    Password:
+                    <input />
+                </label>
+            </form>
+            
+        </div>
+    );
+};
+
+
+export default Login;
