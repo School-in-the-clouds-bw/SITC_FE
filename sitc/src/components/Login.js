147f4dfbad732845bc493d78bf1551d9757afce2
Asha Gross <ashagross@Ashas-MacBook-Pro.local>
2020-07-28 23:00:36 -0400

    Login Completed

--- sitc/src/components/Login.js
+++ sitc/src/components/Login.js
@@ -1,23 +1,75 @@
 import React, { useState }from 'react';
 import styled from 'styled-components';
 import * as yup from 'yup';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory  } from 'react-router-dom';
 
-
-
-// const schema = yup.object().shape({
+// const formSchema = yup.object().shape({
 //     username: yup.string().required().min(3, 'Invalid Username'),
 //     password: yup.string().string().email()    
 // })
 
+const defaultFormState = {
+    username: '',
+    password: '',
+}
 
+const defaultErrorState = {
+    username: '',
+    password: ''
+}
+
+
+// const validateChange = event => {
+//     yup
+//         .reach(formSchema, event.target.name)
+//         .validate()
+//         .then(valid => {
+//             setErrors({
+//                 ...errors,
+//                 [event.target.name]: ""
+//             });
+//         })
+//         .catch(err => {
+//             setErrors({
+//                 ...errors,
+//                 [event.target.name]: err.errors[0]
+//             });
+//         });
+// };
 
 const Login = () => {
-
+    const history = useHistory();
+    const [formState, setFormState] = useState(defaultFormState);
     const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [errors, setErrors] = useState(defaultErrorState);
+
+    const handleChange = event=> {
+        console.log(formState)
+        setFormState({
+            ...formState, [event.target.name]: event.target.value
+        })
+    }
 
     const handleSubmit = event => {
         event.preventDefault();
+        axiosWithAuth().post("https://school-in-the-cloud-be.herokuapp.com/api/auth/login", formState)
+        .then(response => {
+            window.localStorage.setItem('token', response.data.token)
+            window.localStorage.setItem('role', response.data.role)
+            console.log(response)
+            if (response.data.role == 'Administrator'){
+                history.push('/adminDashboard')
+            } else if (response.data.role == 'Volunteer'){
+                history.push('/volunteerDash')
+            } else if (response.data.student == 'Student'){
+                history.push('/studentDash')
+            }
+        })
+        .catch(err => console.log('Error from Axios Login Call', err))
     }
+
+    
     return (
 
 
@@ -26,11 +78,11 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <label>
                     Username:
-                    <input type='text' name='username' />
+                    <input type='text' name='username'onChange={handleChange}  value={formState.username}/>
                 </label>
                 <label>
                     Password:
-                    <input type='text' name='password' />
+                    <input type='text' name='password' onChange={handleChange} value={formState.password} />
                 </label>
                 <button type='submit' disabled={buttonDisabled}>Login</button>
             </form>
