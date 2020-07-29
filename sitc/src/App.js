cfc21b5579055b41b799d2cd82a5742580bc46ad
Chris Forrest <redrider_cr@yahoo.com>
2020-07-28 20:38:26 -0400

    router in app.js setup and working

--- sitc/src/App.js
+++ sitc/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import VolunteerDash from './components/VolunteerDash';
 import './App.css';
 
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import NewUserEdit from './components/NewUserEdit';
 import UserEdit from './components/UserEdit'
 
 import Login from './components/Login'
 import AdminDashboard from './components/AdminDashboard';
+import EditTask from './components/EditTask';
+import PrivateRoute from './components/PrivateRoute';
+import SignUp from './components/SignUp';
 
 
 function App() {
@@ -15,13 +18,27 @@ function App() {
     <div className="App">
 
       <Router>
-      <VolunteerDash/>
+        <h1>Welcome to School In The Clouds</h1>
+     {/* <VolunteerDash/>
       <NewUserEdit/>
-      <UserEdit/>
+     <UserEdit/> */}
+      <Link to='/login'>Login</Link>
+      <Link  to='/signUp'>Sign Up</Link>
+      {/*<AdminDashboard />
+      <EditTask /> */}
+
+      <Switch>
+        <PrivateRoute exact path='/volunteerDash' component={ VolunteerDash} />
+        <PrivateRoute exact path='/newUserEdit' component={ NewUserEdit} />
+        <PrivateRoute exact path='/userEdit' component={ UserEdit} />
+        <PrivateRoute exact path='/adminDashboard' component={ AdminDashboard} />
+        <PrivateRoute exact path='/editTask' component={ EditTask } />
+        <Route path='/login' component={Login} />
+        <Route path='/signUp' component={ SignUp } />
+      </Switch>
       </Router>
 
-      <Login />
-      <AdminDashboard />
+     
 
     </div>
   );
--- sitc/src/components/TaskCard.js
+++ sitc/src/components/TaskCard.js
@@ -1,30 +1,51 @@
 import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
+const initialTask = {
+    taskName:'',
+    taskDescriptiong:''
+}
 
 const TaskCard = () => {
-    const [ showTasks, setShowTasks ] = useState(true);
+  
+    const history = useHistory();
+   // const { id } = useParams();
 
-    const editTask = e => {
-        e.preventDefault();
-       
-
+    const editTask = task => {
+       // history.push('/editTask')
     }
+
+   
 
     const deleteTask = e => {
         e.preventDefault();
-
+    /*    axiosWithAuth()
+        .delete(`https://school-in-the-cloud-be.herokuapp.com/api/admin/tasks/${task.id}`)
+        .then( res => {
+            const changedTasks = tasks.filter((task) => {
+                if(task.id !== res.data){
+                    return task
+                }
+            });
+            updateTaskList(changedTasks)
+        })
+        .catch( err => console.log('error deleting task', err))     */
     }
-
+    
 
     return(
         <div className='taskCard'> 
              <h2>Current Tasks</h2>
                 <ul>
+                     
                     <li>we will map tasks from the backend and make a list using the taskId</li>
                     <li>if admin wants to edit the task they will be pushed to the edit task or it will bring up the edit task component</li>
                     <li>or it will bring up a form on this page to edit the task i'm undecided</li>
-                    <button>Edit Task</button>
-                    <button onClick={() => setShowTasks}>Cancel</button>
-                    <button>Delete</button>
+                    <button onClick={editTask} >Edit Task</button>
+                    <button onClick={deleteTask} style={{backgroundColor: 'red',color:'white'}}>Delete</button>
+
                 </ul>
         </div>
     )
