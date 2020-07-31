1773d9a1ca0a3fcbcc637e00ab7f431c6ea62ab4
Asha Gross <ashagross@Ashas-MacBook-Pro.local>
2020-07-30 20:55:43 -0400

    Styling Component

--- sitc/src/App.js
+++ sitc/src/App.js
@@ -2,15 +2,9 @@ import React, { useState } from "react";
 import VolunteerDash from "./components/VolunteerDash";
 import "./App.css";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import UserEdit from "./components/UserEdit";
-
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import NewUserEdit from './components/NewUserEdit';
 import UserEdit from './components/UserEdit'
 
-import Login from './components/Login'
-import AdminDashboard from './components/AdminDashboard';
 import EditTask from './components/EditTask';
 import PrivateRoute from './components/PrivateRoute';
 import SignUp from './components/SignUp';
@@ -37,7 +31,6 @@ function App() {
         <VolunteerDash />
         <UserEdit addDay={addDay} addNewDay={addNewDay} addTime={addTime} addNewTime={addNewTime}/>
         {/* <Route exact path="/editprofile" component={UserEdit}/> */}
-      </Router>
 
       <Login />
       <AdminDashboard />
@@ -53,7 +46,6 @@ function App() {
 
       <Switch>
         <PrivateRoute exact path='/volunteerDash' component={ VolunteerDash} />
-        <PrivateRoute exact path='/newUserEdit' component={ NewUserEdit} />
         <PrivateRoute exact path='/userEdit' component={ UserEdit} />
         <PrivateRoute exact path='/adminDashboard' component={ AdminDashboard} />
         <PrivateRoute exact path='/editTask' component={ EditTask } />
